2f5985269aebd2af58e6ec81ce6d09b4841d8ae2
rickslick3 <richard.robertsf@gmail.com>
2025-04-20 23:27:23 -0400

    adding characters word counts and how man episodes they appear in

--- preprocessing.py
+++ preprocessing.py
@@ -5,32 +5,59 @@ from collections import Counter
 def run_all_text_to_csv_files():
 
     transcripts_dir = "./transcripts"
-    allowed = {"1"}
+    seasons_to_process = {"1"}
 
-    character_speaks_counts = Counter()
+    character_lines_counts = Counter()
+
+    character_words_counts = Counter()
+
+    character_episode_counts = Counter()
     
     # loop through each file in each season directory
     for season in os.listdir(transcripts_dir):
-        if season not in allowed:
+        if season not in seasons_to_process:
             continue
 
         season_dir = os.path.join(transcripts_dir, season)
         if os.path.isdir(season_dir):
             for file_name in os.listdir(season_dir):
-                file_path = os.path.join(season_dir, file_name)
-                if os.path.isfile(file_path):
+                file_path = os.path.join(season_dir, file_name)                    
                     
-                    # in the current file, count how many times each character speaks
-                    with open(file_path, "r", encoding="utf-8") as f:
-                        for line in f:
-                            if ":" in line:
-                                # extract the character's name and normalize it
-                                name = line.split(":", 1)[0].strip().lower()
-                                clean_name = re.sub(r'\([^)]*\)', '', name).strip()
-                                if clean_name:
-                                    character_speaks_counts[clean_name] += 1
-    
-    for character, count in character_speaks_counts.most_common(20):
+                # in the current file, process different counts
+                seen_in_this_file = set()
+                with open(file_path, "r", encoding="utf-8") as f:
+                    for line in f:
+                        
+                        # only process speaking lines, ex: "name: this is their line"
+                        if ":" in line:
+                            # extract the character's name and normalize it
+                            name = line.split(":", 1)[0].strip().lower()
+                            clean_name = re.sub(r'\([^)]*\)', '', name).strip()
+                            
+                            if clean_name:
+                                # update the counts for characters lines
+                                character_lines_counts[clean_name] += 1
+
+                                # update the counts for characters words
+                                words = line.split(":")[1].strip()
+                                words_count = len(words.split())
+                                character_words_counts[clean_name] += words_count
+
+                                # update the counts for characters episode appearances
+                                if clean_name not in seen_in_this_file:
+                                    seen_in_this_file.add(clean_name)
+                                    character_episode_counts[clean_name] += 1
+
+    print("\nCharacter Lines Counts\n")
+    for character, count in character_lines_counts.most_common(15):
+        print(f"{character}: {count}")
+
+    print("\nCharacter Words Counts\n")
+    for character, count in character_words_counts.most_common(15):
+        print(f"{character}: {count}")
+
+    print("\nCharacter Episode Counts\n")
+    for character, count in character_episode_counts.most_common(15):
         print(f"{character}: {count}")
 
 
